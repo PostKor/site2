9d878beafa8002bb6db4315a071b6def503de5eb
lllMaKsImlll <148184294+lllMaKsImlll@users.noreply.github.com>
2024-09-13 16:30:49 +0500

    beta v0.2

---
+++ index.php
@@ -0,0 +1,12 @@
+{# phpblock #}
+
+{% block last_products %}
+
+{% for product in products %}
+    * Name: {{ product.name|e }}
+    {% if product.status === "Active" %}
+       Price: {{ product.price + product.taxes/100 }}
+    {% endif  %}
+{% endfor  %}
+
+{% endblock %}
